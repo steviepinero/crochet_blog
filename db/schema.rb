27fd740c6c02c9dd4f7dd3ab9d12cf22995c331b
Stevie <steviepinero@gmail.com>
2016-02-22 10:52:41 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160222001612) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
