c7058f32b765427798f64657691a1413eea532e6
Stevie <steviepinero@gmail.com>
2016-02-22 11:00:13 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'pictures#index'
+get '/pictures/new' => 'pictures#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
