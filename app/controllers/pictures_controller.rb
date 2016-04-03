84a5155f938942b131c55bb4c4cabf6115c9e458
Stevie <steviepinero@gmail.com>
2016-04-02 20:10:16 -0400

    show user text

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -70,6 +70,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:title, :url)
+      params.require(:picture).permit(:title, :url, :text)
     end
 end
