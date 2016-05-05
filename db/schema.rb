57446238ee100be2c85840b178e1ec9b16962e0b
Stevie <steviepinero@gmail.com>
2016-05-05 18:29:23 -0400

    upvote form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222001612) do
+ActiveRecord::Schema.define(version: 20160502170510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160222001612) do
     t.string   "title"
     t.string   "url"
     t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "upvote_downvotes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
